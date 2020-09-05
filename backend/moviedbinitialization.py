5d0e129a5e63f1969137733d64d6deac348a5a2b
piotrlgol <piopiotrek008@gmail.com>
2020-09-05 16:56:50 +0200

    class for initializing db with movie data

---
+++ backend/moviedbinitialization.py
@@ -0,0 +1,40 @@
+import requests
+
+from backend import keyvalue
+
+class NotEnougthMovies(Exception):
+    def __init__(self, message="There is not enougth movies for given search key"):
+        self.message = message
+    def __str__(self):
+        return repr(self.message)
+
+class MovieDBInitialization():
+    @classmethod
+    def insertmovies(cls, search_key, movies_to_find):
+        if(len(keyvalue.KeyValue.query().fetch()) != 0):
+            return
+        page = 1
+        while len(keyvalue.KeyValue.query().fetch()) < movies_to_find:
+            request = cls.request_movies(search_key, page)
+            if(request['Response'] == 'False'):
+                raise NotEnougthMovies()
+            for movie in request['Search']:
+                keyvalue.set(movie['Title'], movie)
+                if(len(keyvalue.KeyValue.query().fetch()) >= movies_to_find):
+                    break
+            page += 1      
+
+    @classmethod
+    def request_movies(cls, search, page):
+        URL = 'http://www.omdbapi.com/'
+        PARAMS = {'apikey':'74fd942e', 's':search, "page":page}
+        try:
+            response = requests.get(url = URL, params = PARAMS) 
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
+        return response.json()
+
+
+def initializeDB(search_key="space", movies_to_find=100):
+    MovieDBInitialization.insertmovies(search_key, movies_to_find)
---
+++ backend/test/test_moviedbinitialization.py
@@ -0,0 +1,21 @@
+from backend import test
+from backend import moviedbinitialization
+from backend import keyvalue
+
+class TestMovieDBInitialization(test.TestCase):
+    def test_initializeDB(self):
+        self.assertEqual(len(keyvalue.KeyValue.query().fetch()), 0)
+        moviedbinitialization.initializeDB()
+        self.assertEqual(len(keyvalue.KeyValue.query().fetch()), 100)
+
+    def test_db_already_initialized(self):
+        keyvalue.set("key", "value")
+        self.assertEqual(len(keyvalue.KeyValue.query().fetch()), 1)
+        moviedbinitialization.initializeDB()
+        self.assertEqual(len(keyvalue.KeyValue.query().fetch()), 1)
+
+    def test_not_enougth_movies(self):
+        with self.assertRaises(moviedbinitialization.NotEnougthMovies):
+            moviedbinitialization.initializeDB("raiders of the lost ark")
+
+
